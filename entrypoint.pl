5c0c5be7b233000bc1e4da59d4f03ce09ba2d26d
Ricky Cook <rcook@odecee.com.au>
2015-01-08 15:10:10 +1100

    Handle entrypoint args

--- entrypoint.pl
+++ entrypoint.pl
@@ -3,17 +3,28 @@
 use strict;
 use IO::Socket;
 
-my $sock = new IO::Socket::INET (
-    LocalPort => '7000',
-    Proto => 'tcp',
-    Listen => 1,
-    Reuse => 1,
-);
-die "Could not create socket: $!\n" unless $sock;
+if ($ARGV[0] eq 'ci') {
+    print "Ain't nobody here but us chickens\n";
+    exit 0;
 
-while(1) {
-    my $conn = $sock->accept();
-    while(<$conn>) {
-        $conn->write($_);
+} elsif ($ARGV[0] eq 'run') {
+    my $sock = new IO::Socket::INET (
+        LocalPort => '7000',
+        Proto => 'tcp',
+        Listen => 1,
+        Reuse => 1,
+    );
+    die "Could not create socket: $!\n" unless $sock;
+
+    print "Entering event loop\n";
+    while(1) {
+        my $conn = $sock->accept();
+        while(<$conn>) {
+            $conn->write($_);
+        }
     }
+    exit 0;
+
+} else {
+    die "I don't know how to ${ARGV[0]}\n";
 }
